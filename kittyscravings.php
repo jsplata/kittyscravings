d28c91d66dc043f5aad39d8f09ff1f6bcded736c
jsplatar <30661357+jsplatar@users.noreply.github.com>
2020-07-07 12:06:20 -0500

    added composer

---
+++ kittyscravings.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>@kittyscravings</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+</head>
+
+<body>
+  <div class="jumbotron text-center">
+    <h1 class="display-1">Kitty's Cravings</h1>
+    <p>Good food. Good mood.</p>
+    <p style="text-align: center;">TORTAS COMPLETAS MOLDE (24CM)</p>
+    <p style="text-align: center;">10 porciones</p>
+  </div>
+  <div class="container">
+    <p style="text-align: center;">1. BRUCE CAKE</p>
+    <p style="text-align: center;">Torta de chocolate y cacao 70% súper moist, relleno y cubierto por garnache de
+      nutella.</p>
+    <p style="text-align: center;">Torta completa: $75.000</p>
+    <p style="text-align: center;">Slice: $9.000</p>
+    </br>
+    <p style="text-align: center;">2. GOLDEN CARROT CAKE</p>
+    <p style="text-align: center;">Torta de zanahoria y nueces de dos capas, relleno y cubierto de frosting de queso
+      crema.</p>
+    <p style="text-align: center;">Torta completa: $70.000</p>
+    <p style="text-align: center;">Slice: $9.000</p>
+    </br>
+    <p style="text-align: center;">3. TRES LECHES</p>
+    <p style="text-align: center;">Torta húmeda en salsa tres leches.</p>
+    <p style="text-align: center;">Torta completa: $55.000</p>
+    <p style="text-align: center;">Slice: $9.000</p>
+    </br>
+    <p style="text-align: center;">4. TRES LECHES DE COCO</p>
+    <p style="text-align: center;">Torta húmeda en salsa tres leches con sabor a coco.</p>
+    <p style="text-align: center;">Torta completa: $60.000</p>
+    <p style="text-align: center;">Slice: $9.000</p>
+    </br>
+    <p style="text-align: center;">5. ENYUCADO</p>
+    <p style="text-align: center;">Caja X6 enyucados</p>
+    <p style="text-align: center;">$14.000</p>
+    </br>
+    <p style="text-align: center;">*Estos valores no incluyen costos de envío.</p>
+    </br>
+  </div>
+  <div class="col text-center">
+    <button type="button" class="btn btn-primary btn-lg">MAKE YOUR ORDER HERE</button>
+  </div>
+  </br>
+  <div class="container">
+    <table class="borderless table">
+      <tr>
+        <td>Seleccionar postre:</td>
+        <td>Slice: 9K</td>
+      </tr>
+      <div class="form-check">
+        <tr>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1"
+              checked>
+            <label class="form-check-label" for="exampleRadios1">
+              Bruce Cake (75)
+            </label></td>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios11" value="option7"
+              checked>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select> slices</td>
+      </div>
+      </tr>
+      <div class="form-check">
+        <tr>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+              Carrot Cake (70)
+            </label></td>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios22" value="option8"
+              checked>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select> slices</td>
+      </div>
+      </tr>
+      <div class="form-check">
+        <tr>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+            <label class="form-check-label" for="exampleRadios3">
+              Gold Digger Cake (65)
+            </label></td>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios33" value="option9"
+              checked>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select> slices</td>
+      </div>
+      </tr>
+      <div class="form-check">
+        <tr>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios4" value="option4">
+            <label class="form-check-label" for="exampleRadios4">
+              3 leches tradicional (55)
+            </label></td>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios44" value="option10"
+              checked>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select> slices</td>
+      </div>
+      </tr>
+      <div class="form-check">
+        <tr>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios5" value="option5">
+            <label class="form-check-label" for="exampleRadios5">
+              Bart's Head (70)
+            </label></td>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios55" value="option11"
+              checked>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select> slices</td>
+      </div>
+      </tr>
+      <div class="form-check">
+        <tr>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios6" value="option6">
+            <label class="form-check-label" for="exampleRadios6">
+              Lunar Cake (XX)
+            </label></td>
+          <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios66" value="option12"
+              checked>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+            </select> slices</td>
+      </div>
+      </tr>
+    </table>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="ws" id="defaultCheck1">
+      <label class="form-check-label" for="defaultCheck1">
+        Birthday cake?
+      </label>
+    </div>
+
+    <h3>Weekend Special</h3>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="ws" id="defaultCheck1">
+      <label class="form-check-label" for="defaultCheck1">
+        Enyucado box
+      </label>
+    </div>
+
+    <h3>Date</h3>
+    <div class="form-check">
+      <input type="text" id="basicDate" data-input>
+    </div>
+
+    <h3>Time</h3>
+    <div class="form-check">
+      <input type="text" id="basicTime" data-input>
+    </div>
+
+    <script>$("#basicDate").flatpickr({
+        "disable": [
+          function (date) {
+
+            // return true to disable
+            return (date.getDay() === 0);
+          }, '2020-07-20', '2020-08-07', '2020-08-17', '2020-10-12', '2020-11-02', '2020-11-16', '2020-12-08', '2020-12-25', '2021-01-01'
+        ]
+      });</script>
+    <script>$("#basicTime").flatpickr({
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+        minTime: "10:00",
+        maxTime: "18:00",
+      });</script>
+
+    <h3>Forma de pago (Payment)</h3>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="paymentM" id="paymentM1" value="option1">
+      <label class="form-check-label" for="paymentM1">
+        Efectivo (Contraentrega)
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="paymentM" id="paymentM2" value="option2">
+      <label class="form-check-label" for="paymentM2">
+        Transferencia bancaria (Davivienda - Daviplata - Bancolombia - Nequi)
+      </label>
+    </div>
+    <h3>Correo</h3>
+    <div class="form-check">
+      <input class="form-check-input" type="email">
+    </div>
+    <br>
+    <button type="button" class="btn btn-primary">Enviar</button>
+</body>
+
+</html>
